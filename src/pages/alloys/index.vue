720eed1914b12a0a9099e005aaa697b79cec51ca
Oleg Oleynikov <ooleg-1999@list.ru>
2024-04-28 02:33:16 +0300

    start alloy calculator

---
+++ src/pages/alloys/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-row justify="start">
+      <v-col cols="4">
+        <alloy-calculator></alloy-calculator>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
